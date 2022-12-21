d6014951a9edd7bf26591e8faf475f6f227f9b0c
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-21 15:56:17 -0300

    Initial Commit

---
+++ LuCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LuCpp
+//
+//  Created by Marcelo Sampaio on 21/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
